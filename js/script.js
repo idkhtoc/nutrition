03ac1b1c3489c32e75479d3592cdd287f04668ee
Alex <older.miller.ua@gmail.com>
2022-02-22 14:43:05 +0200

    Calculator update

--- js/script.js
+++ js/script.js
@@ -461,9 +461,21 @@ window.addEventListener('DOMContentLoaded', () => {
     // Calculator
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female',
-        height, weight, age,
+    let sex, height, weight, age, ratio;
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
         ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
 
     function calcTotal() {
         if (!sex || !height || !weight || !age || !ratio) {
@@ -480,6 +492,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
     calcTotal();
 
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            }
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
     function getStaticInfo(parentSelector, activeClass) {
         const elements = document.querySelectorAll(`${parentSelector} div`);
 
@@ -487,8 +516,10 @@ window.addEventListener('DOMContentLoaded', () => {
             element.addEventListener('click', event => {
                 if (event.target.getAttribute('data-ratio')) {
                     ratio = +event.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', +event.target.getAttribute('data-ratio'));
                 } else {
                     sex = event.target.getAttribute('id');
+                    localStorage.setItem('sex', event.target.getAttribute('id'));
                 }
 
                 elements.forEach(elem => {
@@ -509,6 +540,12 @@ window.addEventListener('DOMContentLoaded', () => {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+            if (input.value.match(/\D/g)) {
+                input.style.border = '1px solid red';
+            } else {
+                input.style.border = '';
+            }
+
             switch (input.getAttribute('id')) {
                 case 'height':
                     height = +input.value;

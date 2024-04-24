78b1dd113146539885d7db44756187d11522f9f6
Alex Yanov <older.miller.ua@gmail.com>
2024-04-24 17:56:54 +0200

    Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 'use strict';
 
-let path = require('path');
+const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './js/script.js',
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/js'
-  },
-  watch: true,
+	mode: 'development',
+	target: 'web',
+	entry: './js/script.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build/js'),
+	},
+	watch: true,
 
-  devtool: "source-map",
+	devtool: 'source-map',
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', {
-                debug: true,
-                corejs: 3,
-                useBuiltIns: "usage"
-            }]]
-          }
-        }
-      }
-    ]
-  }
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							[
+								'@babel/preset-env',
+								{
+									debug: true,
+									corejs: 3,
+									useBuiltIns: 'usage',
+								},
+							],
+						],
+					},
+				},
+			},
+		],
+	},
 };
